b7f2563076c1808274f1849434829c0fd71ebbf0
iMorph <iMorpheus@users.noreply.github.com>
2021-12-26 12:11:56 +0900

    Create recommend-tv.py

---
+++ recommend-tv.py
@@ -0,0 +1,86 @@
+#!/usr/bin/Python3
+# Purpose: To collect data so we can confirm whether or not Satoshi's TV meets the users needs
+
+### FUNCTIONS ###
+# ask for the user's TV requriements
+# store the user's TV requriements
+# identify hits
+# idenfity misses
+# list hits
+# list misses
+# calulate the match ratio
+# print the match ratio
+
+### DATA ###
+# TV specs
+# user's TV requriements
+# hits
+# misses
+# match ratio
+
+### QUESTIONS ###
+# Do you want ...
+# Do you want to ...
+# Do you want to watch ...
+# Do you need ...
+
+### WHAT WE ARE GOING TO COLLECT ###
+# resolution: 4K
+# resolution: 8K
+# fps: 60
+# fps: 120
+# screen size: 48"
+# screen type: OLED
+# audio: 5.1
+# audio: 7.1
+# audio: Dolby Atmos
+# audio: 44.1hz/16bit
+# audio: 192.0hz/24bit
+# VOD: Netflix
+# VOD: Amazon Prime
+# VOD: Hulu
+# VOD: DAZN
+# VOD: YouTube
+# VOD: Apple TV+
+# VOD: Twitch
+
+print('Welcome to the TV recommendation system!')
+print('Please answer the following questions to help us recommend the best TV for you.\n')
+input()
+print('Do you want to watch TV in 4K?')
+input()
+print('Do you want to watch TV in 8K?')
+input()
+print('Do you want to watch TV at 60fps?')
+input()
+print('Do you want to watch TV at 120fps?')
+input()
+print('Do you want an OLED screen?')
+input()
+print('Do you want a 48" screen?')
+input()
+print('Do you want 5.1 surround sound?')
+input()
+print('Do you want 7.1 surround sound?')
+input()
+print('Do you want Dolby Atmos?')
+input()
+print('Do you want 44.1hz/16bit audio?')
+input()
+print('Do you want 192.0hz/24bit audio?')
+input()
+print('Do you want to watch Netflix?')
+input()
+print('Do you want to watch Amazon Prime?')
+input()
+print('Do you want to watch Hulu?')
+input()
+print('Do you want to watch DAZN?')
+input()
+print('Do you want to watch YouTube?')
+input()
+print('Do you want to watch Apple TV+?')
+input()
+print('Do you want to watch Twitch?')
+input()
+print('If you answered yes to most of the above questions, you should buy LG\'s OLED 48CXPJA.')
